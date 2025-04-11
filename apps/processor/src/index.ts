8ccb4d86f1b85a40eac3fd4cc8fd557ab086f9df
amansubudhi <amansubudhi12@gmail.com>
2025-04-11 17:22:20 +0530

    Added Processor Service

---
+++ apps/processor/src/index.ts
@@ -0,0 +1,67 @@
+import { Kafka, Partitioners } from "kafkajs";
+import client from "@repo/db/client"
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['kafka1:9092', 'kafka2:9092']
+})
+
+const producer = kafka.producer({
+    createPartitioner: Partitioners.LegacyPartitioner
+})
+
+
+const TOPIC_NAME = 'flow-events'
+const POLL_INTERVAL = 5000;
+
+async function sleep(ms: number) {
+    return new Promise((r) => setTimeout(r, ms))
+}
+
+async function main() {
+    await producer.connect()
+    console.log("Processor service started")
+
+    while (true) {
+        try {
+            const outboxRows = await client.flowRunOutbox.findMany({
+                where: {},
+                take: 10
+            });
+
+            if (outboxRows.length === 0) {
+                await sleep(POLL_INTERVAL);
+                continue;
+            }
+
+            await producer.send({
+                topic: TOPIC_NAME,
+                messages: outboxRows.map((row) => ({
+                    value: JSON.stringify({
+                        flowRunId: row.flowRunId,
+                        stage: 0
+                    })
+                }))
+            })
+
+            await client.flowRunOutbox.deleteMany({
+                where: {
+                    id: {
+                        in: outboxRows.map(row => row.id)
+                    }
+                }
+            })
+
+            console.log(`Produced ${outboxRows.length} messages`);
+        } catch (error) {
+            console.error("Error in processor loop:", error);
+        }
+
+        await sleep(POLL_INTERVAL)
+    }
+}
+
+main().catch((err) => {
+    console.error("Fatal error in processor service:", err);
+    process.exit(1);
+});
